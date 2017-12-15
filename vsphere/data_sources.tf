67fa3e6b349bd4aee1242b2557bf9cfdf3f0df43
Grant Orchard <gorchard@vmware.com>
2017-12-15 12:06:28 +1100

    Added resource pool, hosts, datastore data sources

--- vsphere/data_sources.tf
+++ vsphere/data_sources.tf
@@ -1,4 +1,21 @@
-data "vsphere_datacenter" "regiona01" {
-  name = "${var.datacenter}"
+data "vsphere_datacenter" "this" {
+  count = "${length(var.datacenters)}"
+  name = "${var.datacenters[count.index]}"
 }
 
+data "vsphere_resource_pool" "this" {
+  count = "${length(var.resource_pools)}"
+  name = "${var.resource_pools[count.index]}"
+  datacenter_id = "${data.vsphere_datacenter.this.id}"
+}
+
+data "vsphere_host" "this" {
+  count = "${length(var.hosts)}"
+  name  = "${var.hosts[count.index]}"
+}
+
+data "vsphere_datastore" "this" {
+  count = "${length(var.datastores)}"
+  name  = "${var.datastores[count.index]}"
+  datacenter_id = "${data.vsphere_datacenter.this.id}"
+}
