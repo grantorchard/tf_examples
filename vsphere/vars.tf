fcee910b59d0641af2e2b6584db58e7999a80aee
Grant Orchard <gorchard@vmware.com>
2017-12-15 12:05:46 +1100

    Created variables for the data sources

---
+++ vsphere/vars.tf
@@ -0,0 +1,19 @@
+variable datacenters {
+    type = "list"
+    default = []
+}
+
+variable resource_pools {
+    type = "list"
+    default = []
+}
+
+variable hosts {
+    type = "list"
+    default = []
+}
+
+variable datastores {
+    type = "list"
+    default = []
+}
