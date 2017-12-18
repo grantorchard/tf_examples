4a2d998d6a4303a1a45c6f8ce2f445303fc456ba
Grant Orchard <gorchard@vmware.com>
2017-12-18 20:32:09 +1100

    Added customisation specification values

--- vsphere/vars.tf
+++ vsphere/vars.tf
@@ -1,29 +1,78 @@
+variable user {
+  type    = "string"
+  default = "administrator@vsphere.local"
+}
+
+variable password {
+  type    = "string"
+  default = ""
+}
+
+variable vsphere_server {
+  type    = "string"
+  default = ""
+}
+
+variable allow_unverified_ssl {
+  default = true
+}
+
 variable datacenters {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
 }
 
 variable resource_pools {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
 }
 
 variable hosts {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
 }
 
 variable datastores {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
 }
 
 variable port_groups {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
+}
+
+variable templates {
+  type    = "list"
+  default = []
 }
 
 variable vms {
-    type = "list"
-    default = []
+  type    = "list"
+  default = []
+}
+
+variable virtual_machine_name_prefix {
+  type    = "string"
+  default = "tf-"
+}
+
+variable adapter_type {
+  type    = "string"
+  default = "vmxnet3"
+}
+
+variable ipaddress {
+  type    = "list"
+  default = []
+}
+
+variable gateway {
+  type    = "string"
+  default = ""
+}
+
+variable domain {
+  type    = "string"
+  default = ""
 }
